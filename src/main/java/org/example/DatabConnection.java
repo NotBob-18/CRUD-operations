1d0460c0e122624603ef62bfbaf84b105ddf1a2a
Matukane00 <jabulile.matukane@gmail.com>
2024-06-26 13:22:03 +0200

    stashing Database connection creation - kwazi, mike

--- src/main/java/org/example/DatabConnection.java
+++ src/main/java/org/example/DatabConnection.java
@@ -1,6 +1,36 @@
 package org.example;
+import java.sql.*;
 
 public class DatabConnection {
 
 
+    public static Connection getConnection()  {
+
+
+        String url = "jdbc:postgresql://localhost:5432/Demo";
+        String user = "postgres";
+        String password = "123456";
+        Connection conn = null;
+
+        try {
+            Class.forName(
+                    "org.postgresql.Driver"); // Driver name
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try {
+            Connection con = DriverManager.getConnection(
+                    url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(
+                "Connection was successfully established");
+
+
+        return null;
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,21 @@
 package org.example;
 
+
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            Connection con = DatabConnection.getConnection();
+            if (con == null) {
+                System.out.println("Houston, We're ready to Rock!!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (Exception e) {
 
-
+        }
     }
+
 }
