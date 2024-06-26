59ada68ca7b5f38fbc75fe013e0b570df04082af
Matukane00 <jabulile.matukane@gmail.com>
2024-06-26 16:27:42 +0200

    stashing completed project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,9 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
+//CrudFunc.addTask(5,"sitting in a tree", 2, false);
+//        CrudFunc.deleteFunc(5);
+//CrudFunc.updateTaskById(2, "Eating this bomb ass box fish");
 
-        CrudFunc.deleteFunc( 3);
-
-
+    }
 }
